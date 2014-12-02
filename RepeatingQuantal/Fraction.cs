f3c92a0806631051bd5ef76693ec54f56b014c0f
Dustine Camacho <dustine.camacho@gmail.com>
2014-12-02 18:38:15 +0000

    First proper commit, added math
    Added some math functions and a primitive interface.

---
+++ RepeatingQuantal/Fraction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatingQuantal
+{
+    class Fraction
+    {
+        int Numerator { get; set; }
+        int Denominator { get; set; }
+    }
+}
